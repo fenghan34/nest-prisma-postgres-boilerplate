40c3cec83715f8ff0e8a176ebff9df4bb88f546a
fenghan <fenghan770@gmail.com>
2022-01-12 17:29:56 +0800

    chore: change directory structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
-import { AppModule } from './app.module'
 import { isDev } from './constants'
+import { AppModule } from './modules/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
