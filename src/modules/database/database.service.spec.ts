109644b3a5498d7f915f2264abfc8b85438981a3
Feng Han <hi@fenghan.link>
2022-01-26 11:08:29 +0800

    test: update tests for database service

--- src/modules/database/database.service.spec.ts
+++ src/modules/database/database.service.spec.ts
@@ -2,38 +2,33 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { DatabaseService } from './database.service'
 
 describe('DatabaseService', () => {
-  let service: DatabaseService
-  let mockedConnect: jest.SpyInstance<Promise<void>, []>
-  let mockedDisconnect: jest.SpyInstance<Promise<void>, []>
+  let databaseService: DatabaseService
 
   beforeEach(async () => {
-    mockedConnect = jest
-      .spyOn(DatabaseService.prototype, '$connect')
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      .mockImplementationOnce(async () => {})
-
-    mockedDisconnect = jest
-      .spyOn(DatabaseService.prototype, '$disconnect')
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      .mockImplementationOnce(async () => {})
-
     const module: TestingModule = await Test.createTestingModule({
       providers: [DatabaseService],
     }).compile()
 
-    service = module.get(DatabaseService)
-  })
-
-  afterAll(async () => {
-    mockedConnect.mockRestore()
-    mockedDisconnect.mockRestore()
+    databaseService = module.get<DatabaseService>(DatabaseService)
   })
 
   it('should connect and disconnect', async () => {
-    expect(service).toBeDefined()
+    expect(databaseService).toBeDefined()
 
-    await service.onModuleInit()
-    await service.onModuleDestroy()
+    const mockedConnect = jest
+      .spyOn(DatabaseService.prototype, '$connect')
+      .mockImplementationOnce(async () => {
+        //
+      })
+
+    const mockedDisconnect = jest
+      .spyOn(DatabaseService.prototype, '$disconnect')
+      .mockImplementationOnce(async () => {
+        //
+      })
+
+    await databaseService.onModuleInit()
+    await databaseService.onModuleDestroy()
 
     expect(mockedConnect).toHaveBeenCalledTimes(1)
     expect(mockedDisconnect).toHaveBeenCalledTimes(1)
