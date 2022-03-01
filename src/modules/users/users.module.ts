fa5bd2e3fa4cd9754b159becfda3dff1945a057d
Feng Han <hi@fenghan.link>
2022-03-01 16:41:41 +0800

    feat: users module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
+import { DatabaseModule } from '../database/database.module'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
