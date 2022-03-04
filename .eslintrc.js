d6ad137866fea590ccce3c31bc36bdabe24f8807
Feng Han <hi@fenghan.link>
2022-03-04 13:15:27 +0800

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,42 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'plugin:jest/recommended',
-    'plugin:jest/style',
-  ],
   root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint/eslint-plugin'],
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'dist', 'coverage'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-
-    'jest/expect-expect': [
-      'error',
-      {
-        assertFunctionNames: ['expect', 'request.**.expect'],
+  ignorePatterns: ['dist', 'coverage', 'node_modules'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'plugin:jest/recommended',
+        'plugin:jest/style',
+      ],
+      parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
       },
-    ],
-  },
+      rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { vars: 'all', args: 'after-used', ignoreRestSiblings: true },
+        ],
+
+        'jest/expect-expect': [
+          'error',
+          {
+            assertFunctionNames: ['expect', 'request.**.expect'],
+          },
+        ],
+      },
+    },
+  ],
 }
